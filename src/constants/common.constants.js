d6770d23e878679b492216da4980a0b13846f520
Kolezhniuk <D.Kolezhiniyk@gmail.com>
2023-03-20 14:08:18 +0100

    rm urls

--- src/constants/common.constants.js
+++ src/constants/common.constants.js
@@ -1,5 +1,5 @@
-export const RHS_URL = 'https://rhs-staging.polygonid.me/';
-export const RPC_URL = 'https://polygon-mumbai.g.alchemy.com/v2/BI7_GYAO787OflUC7E6DhMJNkhkyq7kp';
+export const RHS_URL = '';
+export const RPC_URL = '';
 
 export const defaultEthConnectionConfig = {
 	url: RPC_URL,
