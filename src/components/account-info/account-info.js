9c3fbfaf4fbf80745227adba379911c3f21f92f9
benbaessler <mail@benbaessler.com>
2023-10-16 19:50:17 +0100

    Update header design

--- src/components/account-info/account-info.js
+++ src/components/account-info/account-info.js
@@ -1,58 +1,31 @@
-import React from 'react';
-import { hideString } from "../../utils";
-import './styles.css';
-import Tooltip from '@mui/material/Tooltip';
-export const AccountInfo = (props)=> {
-	const currentAccount = props.accounts[0]
-	return (
+import React from "react";
+import LogoImage from "../../assets/logo.png";
+
+import "./styles.css";
+
+export const AccountInfo = (props) => {
+  const currentAccount = props.accounts[0];
+  return (
     <div className={"menu-bar"}>
-      <div className={'account-info'}>
-				<button className="connected-status-indicator" onClick={()=>{}}>
-					<div className={'color-indicator'}>
-						<span className="color-indicator__inner-circle" />
-					</div>
-					<div className="connected-status-indicator__text">Connected</div>
-				</button>
-			</div>
-      <div className={"selected-account"}>
-        <div style={{ display: "inline" }}>
-          <Tooltip disableFocusListener title="Copy to clipboard">
-            <button
-              className="selected-account__clickable"
-              onClick={() => {
-                navigator.clipboard.writeText(currentAccount.did);
-              }}
-            >
-              <div className={"selected-account__name"}>
-                {currentAccount.name}
-              </div>
-              <div className="selected-account__address">
-                {hideString(currentAccount.did, 6, -6)}
-                <div className={"selected-account__copy"}>
-                  <svg
-                    width="11"
-                    height="11"
-                    viewBox="0 0 11 11"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M0 0H1H9V1H1V9H0V0ZM2 2H11V11H2V2ZM3 3H10V10H3V3Z"
-                      fill="#535a61"
-                    ></path>
-                  </svg>
-                </div>
-              </div>
-            </button>
-          </Tooltip>
-        </div>
+      <div className={"account-info"}>
+        <button className="connected-status-indicator" onClick={() => {}}>
+          <div className={"color-indicator"}>
+            <span className="color-indicator__inner-circle" />
+          </div>
+          <div className="connected-status-indicator__text">Connected</div>
+        </button>
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          style={{ height: "25px", marginRight: "5px" }}
+          src={LogoImage}
+          alt={"ShopConnect"}
+        />
+        <h3>ShopConnect</h3>
       </div>
       {/* <div className={'menu-bar__account-options'}>
 				<Icon component={MoreVertIcon} color={'red'} />
 			</div> */}
     </div>
   );
-}
-
+};
