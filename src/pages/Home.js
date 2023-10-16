66fcdce5f6b25bbfe1eda511d3d003f430416b5c
benbaessler <mail@benbaessler.com>
2023-10-16 17:18:32 +0100

    Add tab navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,16 @@ import { AccountInfo } from "../components/account-info";
 import { CredentialsInfo } from "../components/credentials";
 import { useNavigate } from "react-router-dom";
 import { ExtensionService } from "../services/Extension.service";
+import { Tabs, Tab } from "@mui/material";
+import { TabPanel, TabContext } from "@mui/lab";
+
 export const Home = () => {
   const navigate = useNavigate();
   const [accounts, setAccounts] = useState([]);
   const [credentials, setCredentials] = useState([]);
+
+  // Tab state
+  const [tabIndex, setTabIndex] = useState(0);
 
   const getCredentials = async () => {
     const { credWallet } = await ExtensionService.getInstance();
@@ -16,21 +22,21 @@ export const Home = () => {
   };
 
   useEffect(() => {
-	  const accounts = JSON.parse(localStorage.getItem('accounts'));
-	  if(!accounts || accounts.length <= 0) {
-		  navigate('/welcome');
-	  } else {
-		  setAccounts(accounts);
-	  }
+    const accounts = JSON.parse(localStorage.getItem("accounts"));
+    if (!accounts || accounts.length <= 0) {
+      navigate("/welcome");
+    } else {
+      setAccounts(accounts);
+    }
     window.addEventListener("storage", () => {
       console.log("Change to local storage!");
       const accounts = JSON.parse(localStorage.getItem("accounts"));
       setAccounts(accounts ? accounts : []);
-      if(!accounts || accounts.length <= 0) {
-        navigate('/welcome');
+      if (!accounts || accounts.length <= 0) {
+        navigate("/welcome");
       }
     });
-	  getCredentials().catch(console.error);
+    getCredentials().catch(console.error);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -42,15 +48,27 @@ export const Home = () => {
 
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
-	  { accounts.length <=0 && <p>Redirecting...</p> }
-	  { accounts.length > 0 && <div>
-        <AccountInfo accounts={accounts} />
-        <CredentialsInfo
-          credentials={credentials}
-          onDeleteCredential={handleCredentialDelete}
-        />
-	  </div>
-	  }
+      {accounts.length <= 0 && <p>Redirecting...</p>}
+      {accounts.length > 0 && (
+        <div>
+          <AccountInfo accounts={accounts} />
+          <TabContext value={tabIndex}>
+            <Tabs value={tabIndex} onChange={(e, index) => setTabIndex(index)}>
+              <Tab label="Deals" value="1"/>
+              <Tab label="History" value="2"/>
+            </Tabs>
+            <TabPanel value="1" index={0}>
+              <p>Deals</p>
+            </TabPanel>
+            <TabPanel value="2" index={1}>
+              <CredentialsInfo
+                credentials={credentials}
+                onDeleteCredential={handleCredentialDelete}
+              />
+            </TabPanel>
+          </TabContext>
+        </div>
+      )}
     </div>
   );
 };
