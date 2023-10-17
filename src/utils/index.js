7f187ac84ef78a10dc6645a3ff8e6cdc417cd183
benbaessler <mail@benbaessler.com>
2023-10-17 15:52:36 +0100

    Add shadcn dependencies

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,10 @@
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+ 
+export function cn(...inputs) {
+  return twMerge(clsx(inputs))
+}
+
 export const hideString = (input, first = 15, second = -10) => {
 	return `${input.slice(0, first)}…${input.slice(second)}`;
 };
