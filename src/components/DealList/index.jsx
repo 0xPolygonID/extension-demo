4f8dd23a0aaa14969d5698df780edc4b2c027fc0
benbaessler <mail@benbaessler.com>
2023-10-19 20:50:28 +0100

    Redesign deals list

--- src/components/DealList/index.jsx
+++ src/components/DealList/index.jsx
@@ -1,57 +1,73 @@
 import { Badge } from "../ui/badge";
 import { Table, TableBody, TableCell, TableRow } from "../ui/table";
 import { Button } from "../ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export default function DealList() {
   const mockDataA = [
     {
+      image:
+        "https://athome.starbucks.com/sites/default/files/styles/carousel_415x347/public/2022-05/CAH_PDP_Vanilla_1842x1542_Ground_shadow.png.webp?itok=jyAVO26X",
+      title: "Vanta Roast (18oz)",
       discount: "10%",
+      expiry: "3 days",
       description: "Bought coffee in the past week",
       unlocked: true,
     },
     {
+      image: "https://dam.delonghi.com/600x600/assets/223655",
+      title: "Delonghi Magnifica S",
       discount: "$5",
+      expiry: "1 week",
       description: "Recurring customer discount",
-      unlocked: false,
-    },
-    {
-      discount: "25%",
-      description: "Spent over $100 in the past month",
       unlocked: false,
     },
   ];
 
   const mockDataB = [
     {
-      discount: "50%",
-      description: "Spent over $1000 in the past week",
-      unlocked: false,
+      image:
+        "https://athome.starbucks.com/sites/default/files/styles/carousel_415x347/public/2022-05/CAH_PDP_Vanilla_1842x1542_Ground_shadow.png.webp?itok=jyAVO26X",
+      title: "Vanta Roast (18oz)",
+      discount: "10%",
+      expiry: "3 days",
+      description: "Bought coffee in the past week",
+      unlocked: true,
     },
     {
-      discount: "$10",
-      description: "Bought sneakers in the past month",
+      image: "https://dam.delonghi.com/600x600/assets/223655",
+      title: "Delonghi Magnifica S",
+      discount: "$5",
+      expiry: "1 week",
+      description: "Recurring customer discount",
       unlocked: false,
     },
   ];
 
   return (
     <div>
-      <span className="m-4 font-bold">Available deals ({mockDataA.length})</span>
+      <span className="m-4 font-bold">
+        Available deals ({mockDataA.length})
+      </span>
       <Table>
         <TableBody>
           {mockDataA.map((deal, index) => (
             <TableRow key={index}>
-              <TableCell>
-                <Badge variant="outlined">{deal.discount}</Badge>
-              </TableCell>
-              <TableCell sx={{ fontSize: "14px" }}>
-                {deal.description}
-              </TableCell>
-              <TableCell align="right">
-                <Button disabled={deal.unlocked}>
-                  {deal.unlocked ? "Unlocked" : "Unlock"}
-                </Button>
-              </TableCell>
+              <div className="flex p-4 justify-between items-center">
+                <div className="flex space-x-3">
+                  <img
+                    src={deal.image}
+                    alt="deal"
+                    className="w-20 h-20 rounded-lg"
+                  />
+                  <div className="flex flex-col items-start	space-y-1">
+                    <Badge variant="outlined">{deal.discount} OFF</Badge>
+                    <span className="font-bold">{deal.title}</span>
+                    <span>Expires in {deal.expiry}</span>
+                  </div>
+                </div>
+                <Button className="mr-3">Apply</Button>
+              </div>
             </TableRow>
           ))}
         </TableBody>
@@ -61,15 +77,21 @@ export default function DealList() {
         <TableBody>
           {mockDataB.map((deal, index) => (
             <TableRow key={index}>
-              <TableCell>
-                <Badge variant="outlined">{deal.discount}</Badge>
-              </TableCell>
-              <TableCell sx={{ fontSize: "14px" }}>
-                {deal.description}
-              </TableCell>
-              <TableCell align="right">
-                <Button>Explore</Button>
-              </TableCell>
+              <div className="flex p-4 justify-between items-center">
+                <div className="flex space-x-3">
+                  <img
+                    src={deal.image}
+                    alt="deal"
+                    className="w-20 h-20 rounded-lg"
+                  />
+                  <div className="flex flex-col items-start	space-y-1">
+                    <Badge variant="outlined">{deal.discount} OFF</Badge>
+                    <span className="font-bold">{deal.title}</span>
+                    <span>Expires in {deal.expiry}</span>
+                  </div>
+                </div>
+                <Button className="mr-3">Unlock</Button>
+              </div>
             </TableRow>
           ))}
         </TableBody>
