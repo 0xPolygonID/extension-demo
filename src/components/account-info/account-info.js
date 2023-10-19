41c9fa42b10ca9b32cd9d8cc24fb061db9eb0031
benbaessler <mail@benbaessler.com>
2023-10-19 13:29:14 +0100

    Update menu bar

--- src/components/account-info/account-info.js
+++ src/components/account-info/account-info.js
@@ -1,31 +1,24 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png";
+import { Badge } from "../ui/badge";
 
 import "./styles.css";
 
 export const AccountInfo = (props) => {
-  const currentAccount = props.accounts[0];
+  const connectedSite = "nike.com"
+
   return (
     <div className={"menu-bar"}>
       <div className={"account-info"}>
-        <button className="connected-status-indicator" onClick={() => {}}>
-          <div className={"color-indicator"}>
-            <span className="color-indicator__inner-circle" />
-          </div>
-          <div className="connected-status-indicator__text">Connected</div>
-        </button>
+        <Badge variant="secondary" className="flex">
+          <div className="w-1.5 h-1.5 rounded-full bg-green-500 mr-2"/>
+          {connectedSite}
+        </Badge>
       </div>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <img
-          style={{ height: "25px", marginRight: "5px" }}
-          src={LogoImage}
-          alt={"ShopConnect"}
-        />
-        <h3>ShopConnect</h3>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <img className="h-6 mr-2" src={LogoImage} alt={"ShopConnect"} />
+        <h3 className="text-lg font-bold">ShopConnect</h3>
       </div>
-      {/* <div className={'menu-bar__account-options'}>
-				<Icon component={MoreVertIcon} color={'red'} />
-			</div> */}
     </div>
   );
 };
