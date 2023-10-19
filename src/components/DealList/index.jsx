746abf0510900706bdfe09f11d846bd2bf87c6a2
benbaessler <mail@benbaessler.com>
2023-10-19 21:05:27 +0100

    Add accordion component

--- src/components/DealList/index.jsx
+++ src/components/DealList/index.jsx
@@ -1,7 +1,14 @@
 import { Badge } from "../ui/badge";
-import { Table, TableBody, TableCell, TableRow } from "../ui/table";
+import { Table, TableBody, TableRow } from "../ui/table";
 import { Button } from "../ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "../ui/accordion";
+import { Checkbox } from "../ui/checkbox"
+
 
 export default function DealList() {
   const mockDataA = [
@@ -53,21 +60,40 @@ export default function DealList() {
         <TableBody>
           {mockDataA.map((deal, index) => (
             <TableRow key={index}>
-              <div className="flex p-4 justify-between items-center">
-                <div className="flex space-x-3">
-                  <img
-                    src={deal.image}
-                    alt="deal"
-                    className="w-20 h-20 rounded-lg"
-                  />
-                  <div className="flex flex-col items-start	space-y-1">
-                    <Badge variant="outlined">{deal.discount} OFF</Badge>
-                    <span className="font-bold">{deal.title}</span>
-                    <span>Expires in {deal.expiry}</span>
-                  </div>
-                </div>
-                <Button className="mr-3">Apply</Button>
-              </div>
+              <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                  <AccordionTrigger>
+                    <div className="w-full flex p-4 justify-between items-center">
+                      <div className="flex space-x-3">
+                        <img
+                          src={deal.image}
+                          alt="deal"
+                          className="w-20 h-20 rounded-lg"
+                        />
+                        <div className="flex flex-col items-start	space-y-1">
+                          <Badge variant="outlined">{deal.discount} OFF</Badge>
+                          <span className="font-bold">{deal.title}</span>
+                          <span>Expires in {deal.expiry}</span>
+                        </div>
+                      </div>
+                      <Button className="mr-3">Apply</Button>
+                    </div>
+                  </AccordionTrigger>
+                  <AccordionContent value="item-1">
+                    <div className="pl-3">
+                      <div className="flex items-center space-x-2">
+                        <Checkbox id="terms" disabled checked/>
+                        <label
+                          htmlFor="terms"
+                          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        >
+                          {deal.description}
+                        </label>
+                      </div>
+                    </div>
+                  </AccordionContent>
+                </AccordionItem>
+              </Accordion>
             </TableRow>
           ))}
         </TableBody>
--- src/components/ui/accordion.jsx
+++ src/components/ui/accordion.jsx
@@ -21,7 +21,6 @@ const AccordionTrigger = React.forwardRef(({ className, children, ...props }, re
       )}
       {...props}>
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200"/>
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
